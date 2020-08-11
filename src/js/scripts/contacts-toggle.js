62f50a8713a75c93445c399aa235f6d47d703dd4
kiforks <ks.kifor@gmail.com>
2020-08-11 23:18:55 +0300

    Finished contacts script

--- src/js/scripts/contacts-toggle.js
+++ src/js/scripts/contacts-toggle.js
@@ -1,11 +1,13 @@
 function contactsToggle() {
   let toggleButton = document.querySelectorAll('.contacts__button-toggle');
-  let sliderButton = document.querySelectorAll('.contacts__button');
+  let sliderButtonPrev = document.querySelector('.contacts__button--prev');
+  let sliderButtonNext = document.querySelector('.contacts__button--next');
   let addressList =  document.querySelectorAll('.contacts__address');
   let sliderList =  document.querySelectorAll('.contacts__item--main');
   let thumbsItem = document.querySelectorAll('.contacts__item--thumbs');
 
   toggleButton.forEach(button => {
+
     button.onclick = event => {
       let parentRow = event.target.closest('.contacts__address');
 
@@ -19,34 +21,33 @@ function contactsToggle() {
     };
   })
 
-  function sliderToggle() {
+  function sliderToggle(prev, thumb) {
     sliderList.forEach((slider, index) => {
       if(sliderList[index].classList.contains('contacts__item--active')) {
-        if(addressList[index].classList.contains('contacts__address--active')) {
-          addressList[index].classList.remove('contacts__address--active');
+        addressList.forEach(address => {
+          address.classList.remove('contacts__address--active');
+        });
+
+        if(!prev && !thumb) {
+          addressList[index + 1].classList.add('contacts__address--active');
+        } else if(prev && !thumb) {
+          try {
+            addressList[index - 1].classList.add('contacts__address--active');
+          } catch {
+            return;
+          }
         } else {
           addressList[index].classList.add('contacts__address--active');
         }
-      } else {
-        addressList[index].classList.remove('contacts__address--active');
       }
     })
   }
 
-  sliderButton.forEach(button => {
-    button.onclick = event => {
-      event.preventDefault();
-
-      sliderToggle();
-    }
-  })
+  sliderButtonNext.onclick = () => sliderToggle();
+  sliderButtonPrev.onclick = () => sliderToggle(true);
 
   thumbsItem.forEach(button => {
-    button.onclick = event => {
-      event.preventDefault();
-
-      sliderToggle();
-    }
+    button.onclick = () => sliderToggle(false, true);
   })
 }
 
